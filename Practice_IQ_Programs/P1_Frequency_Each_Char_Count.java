d78f60178fdccee83dd84f548b0315be159b007b
Mahendher <gannarapu.mahendher99@gmail.com>
2025-10-08 20:50:19 +0530

    Add Programs

--- Practice_IQ_Programs/P1_Frequency_Each_Char_Count.java
+++ Practice_IQ_Programs/P1_Frequency_Each_Char_Count.java
@@ -25,5 +25,18 @@ public class P1_Frequency_Each_Char_Count {
         {
             System.out.println(entry.getKey()+"->"+entry.getValue());
         }
+
+        //Cap Gemini interview question on 08-10-2025
+        //I want to print in non repeating char of first letter.
+        // Example input is "ahendherannarapu"; here non repeat charcters "dpu".
+        // I what to print "d" only
+
+        // Find and print the first non-repeating character
+        for (Map.Entry<Character, Integer> entry : fre.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First non-repeating character: " + entry.getKey());
+                break; // Stop after finding the first one
+            }
+        }
     }
 }
--- Practice_IQ_Programs/P7_Reverse_Last_Word_String.java
+++ Practice_IQ_Programs/P7_Reverse_Last_Word_String.java
@@ -13,3 +13,31 @@ public class P7_Reverse_Last_Word_String {
         System.out.println(String.join(" ", words));
 }
 }
+/* Cap Gemini interview question on 08-10-2025
+//"Java Automation Testing"
+//"Java Automation gnitseT"
+without using predefined methods
+class Main {
+    public static void main(String[] args) {
+        String str = "Java Automation Testing Framework";
+        String[] words = str.split(" ");
+        String rev = "";
+
+        // Get the last word
+        String lastWord = words[words.length - 1];
+
+        // Reverse the last word manually
+        for (int i = lastWord.length() - 1; i >= 0; i--) {
+            rev = rev + lastWord.charAt(i);
+        }
+
+        // Print all words except the last one
+        for (int i = 0; i < words.length - 1; i++) {
+            System.out.print(words[i] + " ");
+        }
+
+        // Print the reversed last word
+        System.out.print(rev);
+    }
+}
+ */
